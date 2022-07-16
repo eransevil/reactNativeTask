b03e33e71f5211de9f5cc8f56ab1311204597f33
Your Name <eransevil2@gmail.com>
2022-07-16 17:40:24 +0300

    finish

--- components/ColorCard.js
+++ components/ColorCard.js
@@ -12,8 +12,10 @@ export default function ColorCard({ color, handleClick, flash }) {
             height: 200,
             width: 200,
             borderWidth: 4,
-            borderRadius: 70,
-
+            borderTopRightRadius: 0,
+            borderTopLeftRadius: 0,
+            borderBottomRightRadius: 0,
+            borderBottomLeftRadius: 0
         },
         flash: {
             backgroundColor: color == 'red' ? 'rgb(255,125, 125)' : color == 'blue' ? 'rgb(125,125, 255)' : color === 'green' ? 'rgb(125, 255,125)' : 'rgb(255, 255, 125)',
@@ -22,30 +24,28 @@ export default function ColorCard({ color, handleClick, flash }) {
 
         },
         red: {
-            backgroundColor: 'red'
-
+            backgroundColor: 'red',
+            borderTopRightRadius: 100
         },
         blue: {
-            backgroundColor: 'blue'
-
+            backgroundColor: 'blue',
+            borderBottomRightRadius: 100,
+            
         },
         green: {
-            backgroundColor: 'green'
+            backgroundColor: 'green',
+            borderTopLeftRadius: 100,
 
         },
         yellow: {
-            backgroundColor: 'yellow'
-
+            backgroundColor: 'yellow',
+            borderBottomLeftRadius: 100
         },
     });
 
 
     return (
         <TouchableOpacity onPress={handleClick.bind(this, color)} style={{ ...styles.colorCard, ...styles[`${color}`], ...flash ? styles.flash : '' }}>
-            {/* <TouchableOpacity >
-
-            </TouchableOpacity > */}
-
         </TouchableOpacity>
 
     );
